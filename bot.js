96cd953d9d3d9b7134589bf8aeefeb14a348e52b
Bert <bert@Jarvis.local>
2019-08-16 15:22:22 -0300

    Remove Port number

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const options = {
 // Add URL of your app to env variable or enable Dyno Metadata
 // to get this automatically
 // See: https://devcenter.heroku.com/articles/dyno-metadata
-const url = process.env.APP_URL || 'https://retrogamesbot.herokuapp.com:443';
+const url = process.env.APP_URL || 'https://retrogamesbot.herokuapp.com';
 const bot = new TelegramBot(TOKEN, options);
 
 
