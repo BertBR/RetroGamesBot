617169f9fda378e9aad080e40aeb077c77796e85
Bert <bert@Jarvis.local>
2019-08-17 06:56:41 -0300

    Remove useless junk

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
 const TOKEN = '802150654:AAFx1hczbT2x31udxP_xfvQQ2bw9kCRsZ00';
 const TelegramBot = require('node-telegram-bot-api');
 const url = 'https://retrogamesbot.herokuapp.com';
+const options = {
+	webhook: { 
+		port: 443
+	}
+}
 const bot = new TelegramBot(TOKEN, options);
 
-bot.setWebHook(`${url}`);
+bot.setWebHook(`${url}/bot${TOKEN}`);
 
 
 // Just to ping!
-bot.on('message', function onMessage(msg) {
-  bot.sendMessage(msg.chat.i
 const today = new Date().getDay()
 
 bot.on('message', (msg) => {
