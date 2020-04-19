8b36ffd3ba8387b02400b5b4cd1b5f446c3fdab4
Bert <z0bristbr@gmail.com>
2020-04-19 19:52:09 -0300

    remove today clause

--- src/bot.js
+++ src/bot.js
@@ -5,8 +5,6 @@ const { bot } = require('./web');
 
 const { getAdminList, sortGame } = require('./functions');
 
-const today = new Date().getDay();
-
 bot.on('text', (ctx) => {
   const msg = ctx.message;
 
@@ -39,17 +37,15 @@ bot.on('text', (ctx) => {
 
       // Sortear Games
       if (msg.text === '/sortear@retrogamesbr_bot' && msg.from.id === adminId) {
-        if (today === 6) {
-          while (i < 3) {
-            console.log(i);
-            await sortGame(i, bot, chatId, fileNames, links, arrPhoto);
-            i++;
-          }
-        } else {
-          ctx.reply(
-            'Hoje não é dia de sortear os jogos! Por favor aguarde até sábado.',
-          );
+        while (i < 3) {
+          console.log(i);
+          await sortGame(i, bot, chatId, fileNames, links, arrPhoto);
+          i++;
         }
+      } else {
+        ctx.reply(
+          'Hoje não é dia de sortear os jogos! Por favor aguarde até sábado.',
+        );
       }
     } catch (error) {
       console.error('Deu ruim!', error);
