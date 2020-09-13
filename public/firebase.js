d53bfb1e15e6a99e705b3fa14ed5beb5abac961c
Vinicius <viniciusmvn@pm.me>
2020-09-13 01:03:07 -0300

    feature: remove user register.

--- public/firebase.js
+++ public/firebase.js
@@ -34,35 +34,6 @@ function toggleSignIn() {
   document.getElementById('quickstart-sign-in').disabled = true;
 }
 
-function handleSignUp() {
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-  if (email.length < 4) {
-    alert('Please enter an email address.');
-    return;
-  }
-  if (password.length < 4) {
-    alert('Please enter a password.');
-    return;
-  }
-
-  firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
- 
-    var errorCode = error.code;
-    var errorMessage = error.message;
-
-    if (errorCode == 'auth/weak-password') {
-      alert('The password is too weak.');
-    } else {
-      alert(errorMessage);
-    }
-    console.log(error);
- 
-  });
-
-}
-
-
 function initApp() {
 
   firebase.auth().onAuthStateChanged(function (user) {
