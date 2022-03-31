0dbc294db442b572e1ed90c7cb6673405932b18a
Vinicius <viniciusmvn@pm.me>
2022-03-31 17:43:36 -0300

    fix: set bot launch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import bot from './bot';
 
+bot.launch();
+
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
