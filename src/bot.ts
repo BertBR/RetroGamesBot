88735704348bbae01e62c36d99f465ddf59f2108
Vinicius <viniciusmvn@pm.me>
2022-03-31 18:51:36 -0300

    fix: webhook

--- src/bot.ts
+++ src/bot.ts
@@ -9,8 +9,6 @@ import {
 import IgdbService from './services/igdb.service';
 
 const bot = new Telegraf(process.env.BOT_TOKEN || '');
-
-bot.telegram.setWebhook(`${process.env.WEBHOOK_URL}/${process.env.BOT_TOKEN}`);
 bot.launch({
   webhook: {
     domain: process.env.WEBHOOK_URL,
@@ -23,7 +21,7 @@ console.log(`Webhook set to ${process.env.WEBHOOK_URL}`);
 bot.command('count', async (ctx) => ctx.reply(await getTotalGames(ctx)));
 bot.command('consoles', async (ctx) => ctx.reply(await getTotalSortedByConsole(ctx)));
 bot.command('genres', async (ctx) => ctx.reply(await getTotalSortedByGenre(ctx)));
-bot.command('games', async (ctx) => ctx.replyWithMarkdown(await getTotalSortedGames(ctx), { disable_web_page_preview: true }));
+bot.command('games', async (ctx) => ctx.reply(await getTotalSortedGames(ctx)));
 bot.command('sort', async (ctx) => sortThreeGames(ctx));
 bot.on('inline_query', async (ctx) => new IgdbService(ctx).run(ctx.inlineQuery));
 
